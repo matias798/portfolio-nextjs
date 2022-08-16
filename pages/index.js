66f2bf3486ef1e8150a418e119d8b223fe54c3c1
mati <mati@gmail.com>
2022-08-16 13:30:52 -0300

    added about component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar.js";
 import Home from "./components/Home.js";
+import About from "./components/About.js";
 
 export default function Index() {
   return (
     <div>
       <Navbar />
       <Home/>
+      <About/>
     </div>
   );
 }
