a046e4944b96b7443125234b91270194b65131cc
mati <mati@gmail.com>
2022-08-16 13:30:35 -0300

    added home component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar.js";
+import Home from "./components/Home.js";
 
 export default function Index() {
   return (
     <div>
       <Navbar />
+      <Home/>
     </div>
   );
 }
