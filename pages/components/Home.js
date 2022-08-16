178056e7038e5c797f964ae87c03840e618e98ba
mati <mati@gmail.com>
2022-08-16 15:31:33 -0300

    added home initial configs

--- pages/components/Home.js
+++ pages/components/Home.js
@@ -1,10 +1,35 @@
+// Modules
+import { Button } from "react-bootstrap";
+import Image from "next/image";
+import { BsCloudDownload } from "react-icons/bs";
+
+import Styles from "./../../styles/Home.module.css";
+
+// Image
+import coderCompletedTask from "../../assets/images/coder-completed-task.jpg";
+
 const Home = () => {
-    return (
-      <div>
-      Home
+  return (
+    <>
+      <div className={Styles.HomeContainer}>
+        {/* Left */}
+        <div>
+          {/* text */}
+          <div>👋 Hola, soy matias quiroga</div>
+          {/* title */}
+          <h2>Desarrollador Web</h2>
+          <Button variant="primary" className={Styles.Button}>
+            <BsCloudDownload /> Descargar cv
+          </Button>
+        </div>
+        {/* right */}
+        <div>
+          {/* image */}
+          <Image src={coderCompletedTask} width={250} height={250} alt="logo" />
+        </div>
       </div>
-    );
-  };
-  
-  export default Home;
-  
+    </>
+  );
+};
+
+export default Home;
