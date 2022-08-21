27a8209afbc1a87b24aed8c7b6f3ea0d4a59bfc2
mati <mati@gmail.com>
2022-08-21 16:33:45 -0300

    added favicons in the Head component

--- pages/components/Home.js
+++ pages/components/Home.js
@@ -11,7 +11,29 @@ import coderCompletedTask from "../../assets/images/coder-completed-task.jpg";
 const Home = () => {
   return (
     <>
-      <div className={Styles.HomeContainer}>
+      <Head>
+        <title>Matias quiroga</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
         {/* Left */}
         <div>
           {/* text */}
