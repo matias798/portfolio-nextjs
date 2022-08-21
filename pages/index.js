6e4454f0d723d46f481330125b19a2ff58a597bb
mati <mati@gmail.com>
2022-08-21 16:46:18 -0300

    added loading component

--- pages/index.js
+++ pages/index.js
@@ -6,17 +6,77 @@ import Projects from "./components/Projects.js";
 import Experience from "./components/Experience.js";
 import Skills from "./components/Skills.js";
 import Contact from "./components/Contact.js";
+import Loading from "./components/Loading.js";
 
+import { useEffect, useState } from "react";
 export default function Index() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+
+
+
+    var cursor = document.querySelector('.cursor');
+    var cursorinner = document.querySelector('.cursor2');
+    var a = document.querySelectorAll('a');
+    
+    document.addEventListener('mousemove', function(e){
+      var x = e.clientX;
+      var y = e.clientY;
+      cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
+    });
+    
+    document.addEventListener('mousemove', function(e){
+      var x = e.clientX;
+      var y = e.clientY;
+      cursorinner.style.left = x + 'px';
+      cursorinner.style.top = y + 'px';
+    });
+    
+    document.addEventListener('mousedown', function(){
+      cursor.classList.add('click');
+      cursorinner.classList.add('cursorinnerhover')
+    });
+    
+    document.addEventListener('mouseup', function(){
+      cursor.classList.remove('click')
+      cursorinner.classList.remove('cursorinnerhover')
+    });
+    
+    a.forEach(item => {
+      item.addEventListener('mouseover', () => {
+        cursor.classList.add('hover');
+      });
+      item.addEventListener('mouseleave', () => {
+        cursor.classList.remove('hover');
+      });
+    })
+
+
+
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 2500);
+  }, [loading]);
+
   return (
-    <div>
-      <Navbar />
-      <Home/>
-      <About/>
-      <Projects/>
-      <Experience/>
-      <Skills/>
-      <Contact/>
-    </div>
+    <>
+    <div className="cursor"></div>
+<div className="cursor2"></div>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div>
+          <Navbar />
+          <Home />
+          <About />
+          <Projects />
+          <Experience />
+          <Skills />
+          <Contact />
+        </div>
+      )}
+    </>
   );
 }
