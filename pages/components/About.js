cbfe9b3ecb2441d63ad4d92e2ee93a4d39c5a0e6
mati <mati@gmail.com>
2022-08-21 16:43:12 -0300

    added text and styles to about component

--- pages/components/About.js
+++ pages/components/About.js
@@ -1,10 +1,32 @@
+// Component
+import Image from "next/image";
+
+// Image
+import coderCompletedTask from "../../assets/images/coder-focused.jpg";
+import Styles from "../../styles/Home.module.css";
+
 const About = () => {
-    return (
-      <div>
-      About
+  return (
+    <>
+      <div className={Styles.HomeContainer}>
+        {/* Left */}
+        <span>
+          {/* image */}
+          <Image src={coderCompletedTask} width={500} height={400} alt="logo" />
+        </span>
+        {/* right */}
+
+        <div className={Styles.aboutMeTitle}>
+          {/* title */}
+          <h2>Sobre mi</h2>
+          <p>
+            Proactivo, con habilidades interpersonales que me permiten
+            fácilmente adaptarme a grupos de trabajo.
+          </p>
+        </div>
       </div>
-    );
-  };
-  
-  export default About;
-  
+    </>
+  );
+};
+
+export default About;
