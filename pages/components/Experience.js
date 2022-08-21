6fe68fa585512a1b2c45c6fa70d8b0a5264cff20
mati <mati@gmail.com>
2022-08-21 16:45:26 -0300

    added content to experiene component and styles

--- pages/components/Experience.js
+++ pages/components/Experience.js
@@ -1,10 +1,60 @@
+import Styles from "../../styles/Experience.module.css";
+
+const ExperienceItems = [
+  {
+    title: "Lorem ipsum",
+    workedAt: "Lorem ipsum",
+    date: "2020-2022",
+    place: "Remoto",
+  },
+  {
+    title: "Lorem ipsum",
+    date: "2020-2022",
+    place: "Remoto",
+    workedAt: "Lorem ipsum",
+  },
+  {
+    title: "Lorem ipsum",
+    date: "2020-2022",
+    place: "Remoto",
+    workedAt: "Lorem ipsum",
+  },
+  {
+    title: "Lorem ipsum",
+    date: "2020-2022",
+    place: "Remoto",
+    workedAt: "Lorem ipsum",
+  },
+];
+
+
+
 const Experience = () => {
-    return (
-      <div>
-      Experience
+  return (
+    <div className={Styles.Bg}>
+      <h2 className={Styles.Title}>Experiencia</h2>
+      <hr className={Styles.Line}></hr>
+      <div className={Styles.Container}>
+        {ExperienceItems.map((item) => {
+          return (
+            <div key={Math.random()} className={Styles.Card}>
+              {/* Title */}
+              <div className={Styles.ItemTitle}>{item.title}</div>
+              {/* worked at  */}
+              <div className={Styles.WorkedAt}>{item.workedAt}</div>
+
+              <div className="d-flex justify-content-between pt-2">
+                {/* Date */}
+                <div className={Styles.Date}>{item.date}</div>
+                {/* Place */}
+                <div className={Styles.Place}>{item.place}</div>
+              </div>
+            </div>
+          );
+        })}
       </div>
-    );
-  };
-  
-  export default Experience;
-  
+    </div>
+  );
+};
+
+export default Experience;
