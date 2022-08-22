d3e14d4cccdef1d97bb104a408979263a1da4bd9
mati <mati@gmail.com>
2022-08-22 11:05:16 -0300

    changed about component content

--- pages/components/About.js
+++ pages/components/About.js
@@ -3,26 +3,29 @@ import Image from "next/image";
 
 // Image
 import coderCompletedTask from "../../assets/images/coder-focused.jpg";
-import Styles from "../../styles/Home.module.css";
+import Styles from "../../styles/About.module.css";
 
 const About = () => {
   return (
     <>
-      <div className={Styles.HomeContainer}>
+      <div className={Styles.AboutContainer}>
         {/* Left */}
-        <span>
+        <span className={Styles.ImageContainer}>
           {/* image */}
-          <Image src={coderCompletedTask} width={500} height={400} alt="logo" />
+          <Image src={coderCompletedTask} alt="logo" className={Styles.image} />
         </span>
         {/* right */}
 
-        <div className={Styles.aboutMeTitle}>
+        <div className={Styles.aboutMeText}>
           {/* title */}
           <h2>Sobre mi</h2>
-          <p>
-            Proactivo, con habilidades interpersonales que me permiten
-            fácilmente adaptarme a grupos de trabajo.
-          </p>
+          <span className={Styles.Text}>
+            ¡Hola! Me llamo Matías Quiroga, soy un Desarrollador Web Full Stack (
+            MERN ) en busca de nuevas oportunidades laborales. Me considero una
+            persona proactiva, responsable, con muchas ganas de seguir
+            aprendiendo nuevas habilidades y tecnologías web para mi desarrollo
+            profesional.
+          </span>
         </div>
       </div>
     </>
