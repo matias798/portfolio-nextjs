f1957fbe5461423b24b2c9fe5c7cf0d62ff23307
mati <mati@gmail.com>
2022-08-26 18:05:48 -0300

    Changed 🔁 text content

--- pages/components/About.js
+++ pages/components/About.js
@@ -1,32 +1,32 @@
 // Component
 import Image from "next/image";
 
-// Image
+// Images
 import coderCompletedTask from "../../assets/images/coder-focused.jpg";
+
+// Styles
 import Styles from "../../styles/About.module.css";
 
 const About = () => {
   return (
-    <div id="About">
-      <div className={Styles.AboutContainer}>
-        {/* Left */}
-        <span className={Styles.ImageContainer}>
-          {/* image */}
-          <Image src={coderCompletedTask} alt="logo" className={Styles.image} />
-        </span>
-        {/* right */}
+    <div className={Styles.AboutContainer} id="About">
+      {/* Left */}
+      <span className={Styles.ImageContainer}>
+        {/* image */}
+        <Image src={coderCompletedTask} alt="logo" className={Styles.image} />
+      </span>
+      {/* right */}
 
-        <div className={Styles.aboutMeText}>
-          {/* title */}
-          <h2>Sobre mi</h2>
-          <span className={Styles.Text}>
-            ¡Hola! Me llamo Matías Quiroga, soy un Desarrollador Web Full Stack
-            (MERN) en busca de nuevas oportunidades laborales. Me considero
-            una persona proactiva, responsable, con muchas ganas de seguir
-            aprendiendo nuevas habilidades y tecnologías web para mi desarrollo
-            profesional.
-          </span>
-        </div>
+      <div className={Styles.aboutMeText}>
+        {/* title */}
+        <h2 className="text-start">Sobre mi</h2>
+        <span className={Styles.Text}>
+          ¡Hola! Me llamo Matías Quiroga, soy un Desarrollador Web Full Stack
+          (MERN) en busca de nuevas oportunidades laborales. Me considero una
+          persona proactiva, responsable, con un gran Interés de seguir
+          aprendiendo nuevas habilidades y tecnologías web para mi desarrollo
+          profesional.
+        </span>
       </div>
     </div>
   );
