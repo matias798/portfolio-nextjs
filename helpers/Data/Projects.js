88b5082ba3d94e22edb00165e236467bcab36510
mati <mati@gmail.com>
2022-08-26 18:24:02 -0300

    Added ➕ projects data

---
+++ helpers/Data/Projects.js
@@ -0,0 +1,28 @@
+// Assets
+import rameDeco from "./../../assets/images/rameDeco.png";
+import Glamira from "./../../assets/images/glamira.png";
+import Cobranzas from "./../../assets/images/Cobranzas.png";
+
+export const projects = [
+    {
+      title: "Cobranzas Argentina",
+      subtitle:
+        "Sistema el cual utilizando un archivo csv con los datos de los clientes se envia un mensaje y un pdf predefinido.",
+      image: Cobranzas,
+      color: "#FFEBF4",
+    },
+    {
+      title: "Glamira",
+      subtitle:
+        "Ecommerce enfocado en la venta de ropa y joyeria para hombres y mujeres realizado de forma individual",
+      image: Glamira,
+      color: "#F2EEFF",
+    },
+    {
+      title: "Rame Deco",
+      subtitle:
+        "Ecommerce enfocado en la venta de productos de decoración realizado de forma conjunta en Digital House",
+      image: rameDeco,
+      color: "#e6daed",
+    },
+  ];
