e35891dcba5711f4c77fac6bd74d91cc4d0bdb7a
mati <mati@gmail.com>
2022-08-27 14:28:20 -0300

    Deleted ❌ unused div

--- components/Experience.js
+++ components/Experience.js
@@ -2,14 +2,12 @@
 import Styles from "./../styles/Experience.module.css";
 
 // Data
-import  {ExperienceItems}  from "./../helpers/Data/Experience";
+import { ExperienceItems } from "./../helpers/Data/Experience";
 
 const Experience = () => {
   return (
     <div className={Styles.Bg}>
-      <div>
-        <h2 className={Styles.Title}>Experiencia</h2>
-      </div>
+      <h2 className={Styles.Title}>Experiencia</h2>
       <div className={Styles.Container}>
         {ExperienceItems.map((item) => {
           return (
