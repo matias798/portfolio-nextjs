724850c6eee3b306320fbad0ec7fd76e1cf1f07a
matias798 <matiasquiroga584@gmail.com>
2022-08-31 20:29:36 -0300

    changed project max width

--- components/Projects.js
+++ components/Projects.js
@@ -10,59 +10,63 @@ import Styles from "./../styles/Project.module.css";
 
 // Data
 import { projects } from "./../helpers/Data/Projects";
-
 import { isMobile } from "./../helpers/IsMobile";
 
 const Projects = () => {
   return (
-    <div className={`${Styles.Bg} p-4`}>
-      <h2 className="text-center pb-4">Portafolio</h2>
+    <div className={Styles.Bg}>
+      <div className='g-Container'>
+        <h2 className="text-center pb-4">Portafolio</h2>
 
-      <Swiper
-        modules={[Autoplay, Pagination, Navigation]}
-        autoplay={{
-          disableOnInteraction: true,
-          pauseOnMouseEnter: true,
-          delay: 3500,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        spaceBetween={50}
-        slidesPerView={isMobile?.() ? 1 : 2}
-        className="container"
-      >
-        {projects.map((item) => (
-          <SwiperSlide
-            key={Math.random()}
-            style={{ cursor: "pointer" }}
-            className={Styles.Card}
-          >
-            <Link href={item.link} className={Styles.Link}>
-              <a className={Styles.Link} target="_blank"
-                rel="noopener noreferrer">
-                {/*  image */}
-                <div
-                  className={Styles.Image}
-                  style={{ backgroundColor: item.color }}
+        <Swiper
+          modules={[Autoplay, Pagination, Navigation]}
+          autoplay={{
+            disableOnInteraction: true,
+            pauseOnMouseEnter: true,
+            delay: 3500,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          spaceBetween={50}
+          slidesPerView={isMobile?.() ? 1 : 2}
+          className="container"
+        >
+          {projects.map((item) => (
+            <SwiperSlide
+              key={Math.random()}
+              style={{ cursor: "pointer" }}
+              className={Styles.Card}
+            >
+              <Link href={item.link} className={Styles.Link}>
+                <a
+                  className={Styles.Link}
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
-                  <Image
-                    src={item.image}
-                    alt={item.title}
-                    width={400}
-                    height={200}
+                  {/*  image */}
+                  <div
                     className={Styles.Image}
-                    placeholder="blur"
-                    blurDataURL={item.image}
-                  />
-                </div>
-                <h3 className={Styles.Title}>{item.title}</h3>
-                <p className={Styles.Subtitle}>{item.subtitle}</p>
-              </a>
-            </Link>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+                    style={{ backgroundColor: item.color }}
+                  >
+                    <Image
+                      src={item.image}
+                      alt={item.title}
+                      width={400}
+                      height={200}
+                      className={Styles.Image}
+                      placeholder="blur"
+                      blurDataURL={item.image}
+                    />
+                  </div>
+                  <h3 className={Styles.Title}>{item.title}</h3>
+                  <p className={Styles.Subtitle}>{item.subtitle}</p>
+                </a>
+              </Link>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
   );
 };
