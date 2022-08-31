7e32772d8be4cbab007c2a1fdbe360fb53939f2b
matias798 <matiasquiroga584@gmail.com>
2022-08-31 20:28:39 -0300

    Changed experience order

--- helpers/Data/Experience.js
+++ helpers/Data/Experience.js
@@ -1,30 +1,31 @@
 export const ExperienceItems = [
   {
     title: "Desarrollador Full Stack",
+    date: "4/21 - 6/22 ",
+    place: "Remoto",
+    workedAt: "Zanni",
+    url: "https://zanni-f8324.web.app/",
+  },
+
+  {
+    title: "Desarrollador Front End",
+    date: "6/21 - 7/21 ",
+    place: "Remoto",
+    workedAt: "Hybeans",
+    url: "https://hybeans.com/",
+  },
+  {
+    title: "Desarrollador Full Stack",
     workedAt: "Modo Agrario",
     date: "7/21 - 1/22 ",
     place: "Remoto",
-    url:'https://www.modoagrario.com/'
+    url: "https://www.modoagrario.com/",
   },
   {
     title: "Desarrollador Full Stack",
     workedAt: "Cobranzas Argentina",
     date: "7/21-9/22",
     place: "Remoto",
-    url:'https://cobranzas-argentina.web.app/login'
-  },
-  {
-    title: "Desarrollador Front End",
-    date: "6/21 - 7/21 ",
-    place: "Remoto",
-    workedAt: "Hybeans",
-    url:'https://hybeans.com/'
-  },
-  {
-    title: "Desarrollador Full Stack",
-    date: "4/21 - 6/22 ",
-    place: "Remoto",
-    workedAt: "Zanni",
-    url:'https://zanni-f8324.web.app/'
+    url: "https://cobranzas-argentina.web.app/login",
   },
 ];
