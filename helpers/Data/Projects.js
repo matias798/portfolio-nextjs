9b1029251fd4c95598919dcedda0f622bc1d4b88
matias798 <matiasquiroga584@gmail.com>
2022-08-31 20:44:20 -0300

    added link

--- helpers/Data/Projects.js
+++ helpers/Data/Projects.js
@@ -3,25 +3,28 @@ export const projects = [
     title: "Glamira",
     subtitle:
       "Ecommerce enfocado en la venta de ropa y joyeria para hombres y mujeres realizado de forma individual",
-    image: 'https://res.cloudinary.com/dsoeo0zhi/image/upload/v1661642528/portfolio/Glamira_pzzf6u.png',
+    image:
+      "https://res.cloudinary.com/dsoeo0zhi/image/upload/v1661642528/portfolio/Glamira_pzzf6u.png",
     color: "#FFEBF4",
 
-    link: "https://glamira-frontend.web.app/"
+    link: "https://glamira-frontend.web.app/",
   },
   {
     title: "Rame Deco",
     subtitle:
       "Ecommerce enfocado en la venta de productos de decoración realizado de forma conjunta en Digital House",
-    image: 'https://res.cloudinary.com/dsoeo0zhi/image/upload/v1661642528/portfolio/ramedeco_zij851.png',
+    image:
+      "https://res.cloudinary.com/dsoeo0zhi/image/upload/v1661642528/portfolio/ramedeco_zij851.png",
     color: "#e6daed",
-    link: "https://ramedeco.herokuapp.com/"
+    link: "https://ramedeco.herokuapp.com/",
   },
   {
     title: "Cobranzas Argentina",
     subtitle:
       "Sistema el cual utilizando un archivo csv con los datos de los clientes se envia un mensaje y un pdf predefinido.",
     color: "#F2EEFF",
-    image: 'https://res.cloudinary.com/dsoeo0zhi/image/upload/v1661642526/portfolio/Cobranzas_cjpbyf.png',
-    link:""
+    image:
+      "https://res.cloudinary.com/dsoeo0zhi/image/upload/v1661642526/portfolio/Cobranzas_cjpbyf.png",
+    link: "https://cobranzas-argentina.web.app/login",
   },
 ];
