7e74d2ae0607d37aecd0c692be0743fb4de59e3b
mati <mati@gmail.com>
2022-09-02 11:57:21 -0300

    Changed container max size on pc

--- components/Experience.js
+++ components/Experience.js
@@ -9,27 +9,29 @@ import { ExperienceItems } from "./../helpers/Data/Experience";
 
 const Experience = () => {
   return (
-    <div className={Styles.Bg}>
-      <h2 className={Styles.Title}>Experiencia</h2>
-      <div className={Styles.Container}>
-        {ExperienceItems.map((item) => {
-          return (
-            <Link href={item.url} key={item.id}>
-              <a
-                className={Styles.Card}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <div className={Styles.ItemTitle}>{item.title}</div>
-                <div className={Styles.WorkedAt}>{item.workedAt}</div>
-                <div className={`${Styles.ItemInfo} pt-2`}>
-                  <div>{item.date}</div>
-                  <div>{item.place}</div>
-                </div>
-              </a>
-            </Link>
-          );
-        })}
+    <div className="d-flex justify-content-center">
+      <div className={Styles.Bg}>
+        <h2 className={Styles.Title}>Experiencia</h2>
+        <div className={Styles.Container}>
+          {ExperienceItems.map((item) => {
+            return (
+              <Link href={item.url} key={item.id}>
+                <a
+                  className={Styles.Card}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <div className={Styles.ItemTitle}>{item.title}</div>
+                  <div className={Styles.WorkedAt}>{item.workedAt}</div>
+                  <div className={`${Styles.ItemInfo} pt-2`}>
+                    <div>{item.date}</div>
+                    <div>{item.place}</div>
+                  </div>
+                </a>
+              </Link>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
