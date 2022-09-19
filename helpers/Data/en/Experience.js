bbaed200fdc7a2c53ac9b736a96db449e4869099
mati <mati@gmail.com>
2022-09-19 11:06:15 -0300

    Added experience data in english

---
+++ helpers/Data/en/Experience.js
@@ -0,0 +1,35 @@
+export const ExperienceItemsEn = [
+  {
+    id: 1,
+    workedAt: "Full Stack",
+    date: "4/21 - 6/22 ",
+    place: "Remote",
+    title: "Zanni Inmobiliaria",
+    url: "https://zanni-f8324.web.app/",
+  },
+
+  {
+    id: 2,
+    workedAt: "Front End",
+    date: "6/21 - 7/21 ",
+    place: "Remote",
+    title: "Hybeans",
+    url: "https://hybeans.com/",
+  },
+  {
+    id: 3,
+    workedAt: "Full Stack",
+    title: "Modo Agrario",
+    date: "7/21 - 1/22 ",
+    place: "Remote",
+    url: "https://www.modoagrario.com/",
+  },
+  {
+    id: 4,
+    workedAt: "Full Stack",
+    title: "Cobranzas Argentina",
+    date: "7/21-9/22",
+    place: "Remote",
+    url: "https://cobranzas-argentina.web.app/login",
+  },
+];
