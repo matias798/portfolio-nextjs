42c1fe416a376988b77f4c29066b547392c5d882
mati <mati@gmail.com>
2022-09-19 11:06:34 -0300

    Added project data in english

---
+++ helpers/Data/en/Projects.js
@@ -0,0 +1,29 @@
+export const projectsEn = [
+  {
+    title: "Glamira",
+    subtitle:
+      "Ecommerce focused on the sale of jewelry and clothes for men and women.",
+    image:
+      "https://res.cloudinary.com/dsoeo0zhi/image/upload/v1662146277/portfolio/Glamira_pzzf6u_woe7ga.webp",
+    color: "#FFEBF4",
+
+    link: "https://glamira-frontend.web.app/",
+  },
+  {
+    title: "Rame Deco",
+    subtitle: "Ecommerce focused on the sale of home decoration products",
+    image:
+      "https://res.cloudinary.com/dsoeo0zhi/image/upload/v1662146269/portfolio/ramedeco_zij851_otyuan.webp",
+    color: "#e6daed",
+    link: "https://ramedeco.herokuapp.com/",
+  },
+  {
+    title: "Cobranzas Argentina",
+    subtitle:
+      "System in which, using a csv file with customer data, sends a message and a predefined pdf via whatsapp.",
+    color: "#F2EEFF",
+    image:
+      "https://res.cloudinary.com/dsoeo0zhi/image/upload/v1662146277/portfolio/Cobranzas_cjpbyf_qigvp7.webp",
+    link: "https://cobranzas-argentina.web.app/login",
+  },
+];
